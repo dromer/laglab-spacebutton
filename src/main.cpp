262ad15d3a586c6f3cb6fb6468e48f89d1b1fefa
dreamer <dreamer@puikheid.nl>
2017-03-11 01:20:59 +0100

    use timer instead of delay to not block any other actions

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,23 @@
 
 #define USE_SERIAL Serial
 
+// pins
 const int ledr = 14;
 const int ledg = 13;
 const int ledb = 12;
 const int button = 5;
 const int relay = 16;
 
+// states
 int state;
 int old_state = 0;
 
 // give me your wifi bro
 const char *ssid = "LAG";
 const char *password = "somethingeasytoremember";
+
+// timers
+int checkmillis = millis();
 
 void setup() {
 
@@ -52,59 +57,61 @@ void setup() {
 }
 
 void loop() {
-   old_state = state;
-   state = digitalRead(button);
-   HTTPClient http2;
+    if(millis()-checkmillis >= 3000){
+        // read the button and check the state
+        state = digitalRead(button);
+        HTTPClient http_state;
 
-   if(old_state != state){
-     if(state == 1){
-         http2.begin("state.lag", 80, "/?spacebutton=closespace");
-         http2.GET();
-         USE_SERIAL.print("New button state and closing\n");
-       }
-     else if(state == 0){
-         http2.begin("state.lag", 80, "/?spacebutton=openspace");
-         http2.GET();
-         USE_SERIAL.print("New button state and opening\n");
-       }
-   } else{
-       USE_SERIAL.print("Nothing changed\n");
-   }
+        if(old_state != state){
+         if(state == 1){
+             http_state.begin("state.lag", 80, "/?spacebutton=closespace");
+             http_state.GET();
+             USE_SERIAL.print("New button state and closing\n");
+           }
+         else if(state == 0){
+             http_state.begin("state.lag", 80, "/?spacebutton=openspace");
+             http_state.GET();
+             USE_SERIAL.print("New button state and opening\n");
+           }
+        } else{
+           USE_SERIAL.print("Nothing changed\n");
+        }
 
-   HTTPClient http;
+        old_state = state;
 
-   USE_SERIAL.print("[HTTP] begin...\n");
-   // configure traged server and url
-   http.begin("state.laglab.org", 80, "/botstate"); //HTTP
+        // read back the state set on the page
+        HTTPClient http_read;
 
-   USE_SERIAL.print("[HTTP] GET...\n");
-   // start connection and send HTTP header
-   int httpCode = http.GET();
-   if(httpCode) {
-       // HTTP header has been send and Server response header has been handled
-       USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+        USE_SERIAL.print("[HTTP] begin...\n");
+        // configure traged server and url
+        http_read.begin("state.laglab.org", 80, "/botstate"); //HTTP
 
-       // file found at server
-       if(httpCode == 200) {
-           String payload = http.getString();
-           USE_SERIAL.println(payload);
-           if(payload == "closed"){
-             analogWrite(ledr, 255);
-             analogWrite(ledg, 0);
-             analogWrite(ledb, 0);
-             digitalWrite(relay, LOW);
-             }
-           else if(payload == "open"){
-             analogWrite(ledr, 0);
-             analogWrite(ledg, 255);
-             analogWrite(ledb, 100);
-             digitalWrite(relay, HIGH);
-             }
-       }
-   } else {
-       USE_SERIAL.print("[HTTP] GET... failed, no connection or no HTTP server\n");
-   }
+        USE_SERIAL.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http_read.GET();
+        if(httpCode) {
+           // HTTP header has been send and Server response header has been handled
+           USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-
-delay(3000);
+           // file found at server
+           if(httpCode == 200) {
+               String payload = http_read.getString();
+               USE_SERIAL.println(payload);
+               if(payload == "closed"){
+                 analogWrite(ledr, 255);
+                 analogWrite(ledg, 0);
+                 analogWrite(ledb, 0);
+                 digitalWrite(relay, LOW);
+                 }
+               else if(payload == "open"){
+                 analogWrite(ledr, 0);
+                 analogWrite(ledg, 255);
+                 analogWrite(ledb, 100);
+                 digitalWrite(relay, HIGH);
+                 }
+           }
+        } else {
+           USE_SERIAL.print("[HTTP] GET... failed, no connection or no HTTP server\n");
+        }
+    }
 }
